c8e2f83f835ca2330eef9cb6390f7cd3741ba28b
Adam East <east.adam@icloud.com>
2026-05-18 21:56:11 -0500

    Filter disk mounts to physical devices only, deduplicate

--- server/src/routes/system.js
+++ server/src/routes/system.js
@@ -15,15 +15,17 @@ systemRouter.get('/info', async (req, res) => {
 
 systemRouter.get('/disks', async (req, res) => {
   try {
-    const disks = await si.fsSize();
-    res.json(disks.map((d) => ({
-      fs: d.fs,
-      mount: d.mount,
-      size: d.size,
-      used: d.used,
-      free: d.free,
-      percent: d.use,
-    })));
+    const all = await si.fsSize();
+    const physical = all.filter((d) =>
+      d.fs.startsWith('/dev/') && !d.fs.startsWith('/dev/loop')
+    );
+    const seen = {};
+    for (const d of physical) {
+      if (!seen[d.fs] || d.mount.length < seen[d.fs].mount.length) {
+        seen[d.fs] = { fs: d.fs, mount: d.mount, size: d.size, used: d.used, free: d.free, percent: d.use };
+      }
+    }
+    res.json(Object.values(seen));
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
